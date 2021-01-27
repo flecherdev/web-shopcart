6d1e4a66ee9db60bd00d73d6431843ec519f5bf8
Ezequiel Freire <ezequiel.a.freire@gmail.com>
2021-01-26 23:19:56 -0300

    added material components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,38 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { LayoutComponent } from './layout/layout.component';
+
+// Components Material
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { ListItemComponent } from './list-item/list-item.component';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import {MatTable, MatTableDataSource, MatTableModule} from '@angular/material/table';
+
+// import { MatTableDataSource, MatTable } from '@angular/material';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LayoutComponent,
+    ListItemComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatInputModule,
+    MatButtonModule,
+    MatTableModule,
+    MatTableDataSource,
+    MatTable
   ],
   providers: [],
   bootstrap: [AppComponent]
